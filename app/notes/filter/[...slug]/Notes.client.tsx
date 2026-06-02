bdd564a927ace2619cdc482acdba7f98b4290702
TarasBilyi <taras.bilyi@gmail.com>
2026-06-03 00:13:22 +0300

    made some edits

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -2,16 +2,16 @@
 
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { fetchNotes } from "../../lib/api";
-import NoteList from "../../components/NoteList/NoteList";
-import SearchBox from "../../components/SearchBox/SearchBox";
-import Pagination from "../../components/Pagination/Pagination";
-import NoteForm from "../../components/NoteForm/NoteForm";
-import Modal from "../../components/Modal/Modal";
-import Loader from "../../components/Loader/Loader";
 import { useDebouncedCallback } from "use-debounce";
-import css from "./NotesPage.module.css";
+import css from "../../NotesPage.module.css";
 import { Toaster } from "react-hot-toast";
+import { fetchNotes } from "@/lib/api";
+import SearchBox from "@/components/SearchBox/SearchBox";
+import Pagination from "@/components/Pagination/Pagination";
+import NoteList from "@/components/NoteList/NoteList";
+import Modal from "@/components/Modal/Modal";
+import NoteForm from "@/components/NoteForm/NoteForm";
+import Loader from "@/components/Loader/Loader";
 
 interface NotesClientProps {
   filterTag?: string;
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NotesClient from "../../Notes.client";
+import NotesClient from "./Notes.client";
 
 type Props = {
   params: Promise<{ slug: string[] }>;
