777494d1f39deb9de77b5338d5cdc1da4a29170e
TarasBilyi <taras.bilyi@gmail.com>
2026-06-03 00:39:28 +0300

    edits

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import css from "./page.module.css";
 const NotFound = () => {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>404 - Page not found</h1>
+      <h1 className={css.title}>404 - Page not found!</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
